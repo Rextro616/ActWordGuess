473f5b844a95fc0b618c904a177bbfa3aafaa95f
Rextro <223275@ids.upchiapas.edu.mx>
2023-10-16 09:26:31 -0600

    Cambios en inputs

--- wordguess/src/App.jsx
+++ wordguess/src/App.jsx
@@ -1,46 +1,72 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
 
 class Word {
   constructor(word) {
-    this.word = this.scramble(word);
+    this.word = word;
+    this.wordScrambled = this.scramble(word);
   }
 
-  scramble(word){
-    let wordSplit = word.split('');
-    // word.map((letter) => wordSplit.push(letter));
-    let wordLenght = wordSplit.lenght;
-    return wordSplit;
+  scramble(word) {
+    let wordSplit = word[0].split("");
+    let wordLenght = 5;
+    console.log(wordLenght)
+
+    for (let i = wordLenght - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let tmp = wordSplit[i];
+      wordSplit[i] = wordSplit[j];
+      wordSplit[j] = tmp;
+    }
+
+    return wordSplit
   }
 
 }
 
-
-const getWord = async () => {
-  const baseURL = "https://random-word-api.herokuapp.com/word?length=5";
-
-  const res = await fetch(`${baseURL}`);
-  const data = await res.json();
-
-  let wordGame = new Word(data);
-
-  console.log(wordGame);
-};
-
 function App() {
-
   const [word, setWord] = useState();
+  const [wordScrambled, setWordScrambled] = useState("");
 
   const wordChange = (Event) => {
-    setWord(Event.target.value);
-    console.log(word);
+    setWord(Event.target.value.toLowerCase());
+
+
+  };
+
+  const getWord = async () => {
+    const baseURL = "https://random-word-api.herokuapp.com/word?length=5";
+
+    const res = await fetch(`${baseURL}`);
+    const data = await res.json();
+
+    let wordGame = new Word(data);
+
+    setWordScrambled(wordGame.wordScrambled)
+
+    console.log(wordGame);
   };
 
   return (
-    <main>
-      <input onChange={wordChange} className="entrada-texto" type="text" />
-      <button onClick={getWord}>Restart</button>
+    <main className="main">
+      <h1>{wordScrambled}</h1>
+
+      <div>
+        
+      </div>
+
+      <div className="inputs">
+        <input maxLength={1} onChange={wordChange} className="entrada-texto" type="text" />
+        <input maxLength={1} onChange={wordChange} className="entrada-texto" type="text" />
+        <input maxLength={1} onChange={wordChange} className="entrada-texto" type="text" />
+        <input maxLength={1} onChange={wordChange} className="entrada-texto" type="text" />
+        <input maxLength={1} onChange={wordChange} className="entrada-texto" type="text" />
+      </div>
+
+      <div className="buttons">
+        <button onClick={getWord}>Restart</button>
+      </div>
+
     </main>
   );
 }
